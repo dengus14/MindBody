a15bda33af4882cf27105ae4de94a3fa49ae2dea
dengus14 <ddgusev0318@gmail.com>
2025-10-09 15:00:24 -0500

    user model done

---
+++ src/main/java/com/app/mindbody/controllers/AuthController.java
@@ -0,0 +1,8 @@
+package com.app.mindbody.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
--- src/main/java/com/app/mindbody/models/User.java
+++ src/main/java/com/app/mindbody/models/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
@@ -17,10 +18,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class) // otherwise @CreatedDate and @LastModifiedDate won't populate
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
     private UUID id;
 
     @Column(name = "username", unique = true, nullable = false)
