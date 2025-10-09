a3961bfc649ddf9f326f796cc2bd5894cf750dcd
dengus14 <ddgusev0318@gmail.com>
2025-10-09 14:56:21 -0500

    user model

--- src/main/java/com/app/mindbody/models/User.java
+++ src/main/java/com/app/mindbody/models/User.java
@@ -1,4 +1,45 @@
 package com.app.mindbody.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password_hash;
+
+    @Column(name = "streakCount", nullable = false)
+    private int streak_count;
+
+    @CreatedDate
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(name = "lastLogin", nullable = false)
+    private LocalDateTime last_login;
+
+
+
 }
