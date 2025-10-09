fb699ecde8194710fd895e388768ab50bd8a6019
dengus14 <ddgusev0318@gmail.com>
2025-10-09 14:08:18 -0500

    Initial Spring Boot project commit

---
+++ src/main/java/com/app/mindbody/MindBodyApplication.java
@@ -0,0 +1,13 @@
+package com.app.mindbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindBodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindBodyApplication.class, args);
+    }
+
+}
