2e466a11ba885f5101284806285a3687af3fe5c9
MRSX <alvkondr@gmail.com>
2025-10-30 14:53:42 -0500

    Repositories for auth

---
+++ src/main/java/com/app/mindbody/repositories/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.app.mindbody.repositories;
+
+import com.app.mindbody.models.UserAuth;
+import com.app.mindbody.models.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile,Long> {
+    Optional<UserProfile> findByAuth(UserAuth auth);
+}
